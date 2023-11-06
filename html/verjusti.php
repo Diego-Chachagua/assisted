7e19e7236c4491a0fa4cce184716f5dbca724ed1
JosueAlbanez3 <josuealbanez05gmail.com>
2023-11-06 10:35:30 -0600

    cambio

---
+++ html/verjusti.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="/assisted/css/">
+<div class="ventanaflotante" id="ventana">
+        <div class="contenedor">
+            <br>
+            <p class="escusa">Escriba la justificacion:</p>
+            <textarea type="text" name="justifi" class="chambre"></textarea>
+        </div>
+        <button id="guardar" type="submit">
+              <img class="gua12" src="/assisted/img/chequesito.png">
+              <p class="ca2">Aceptar</p>
+            </button>
+            
+</div>
